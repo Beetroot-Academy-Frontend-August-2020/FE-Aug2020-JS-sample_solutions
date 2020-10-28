2c85a4360a1713f918b5b2408b67c2176e167126
Mina Demian <mina@minademian.com>
2020-10-28 19:56:23 +0100

    refactor: function instead

--- sample-solutions/lesson-28/problem-3.js
+++ sample-solutions/lesson-28/problem-3.js
@@ -1,8 +1,12 @@
 // Write a function that accepts three separate digits and makes 
 // them a number. For example, 1, 4, and 9 will become 149.
 
+function stringFromNumbers(num1, num2, num3) {
+    return num1.concat(num2, num3);
+}
+
 let number1 = prompt('Please input the first number.');
 let number2 = prompt('Please input the second number.');
 let number3 = prompt('Please input the third number.');
 
-console.log(number1.concat(number2, number3));
+console.log(stringFromNumbers(number1, number2, number3));
